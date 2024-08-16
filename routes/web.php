bb775024eee902706eaf8576fad25759519ce55f
ELISCANA <eliscan@paypat.co>
2024-08-16 03:37:05 -1200

    update: project setup done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('ss');
+
+
+Route::controller(FrontendController::class)->group(function(){
+
+    Route::get('/', 'index');
+
 });
+
